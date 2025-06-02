d540c21b491e2f404043a966a97a25c5b3fb4e22
Tim Fuchs <criseone@me.com>
2025-06-02 17:41:47 +0200

    reset the project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <router-view />
 </template>
+
 <script>
-export default { name: 'App' }
+export default {
+  name: 'App',
+}
 </script>
--- src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="p-4">
-    <h1 class="text-xl font-bold mb-4">My Skill Snippets</h1>
-    <!-- Snippets go here -->
-  </div>
-</template>
-<script>
-export default { name: 'Home' }
-</script>
