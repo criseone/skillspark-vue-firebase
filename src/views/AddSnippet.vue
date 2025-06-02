8bdc08e02bc6dc79cb99effe3164d347c0eafa34
Tim Fuchs <criseone@me.com>
2025-06-02 16:17:31 +0200

    Initial commit: SkillSpark prototype with Vue + Firebase

---
+++ src/views/AddSnippet.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-xl font-bold mb-4">Add New Snippet</h1>
+    <!-- Form goes here -->
+  </div>
+</template>
+<script>
+export default { name: 'AddSnippet' }
+</script>
